d386e8e464fe104c3ad2ff1b21e682b2f2bf42c8
Siroshun09 <siro.shun8@gmail.com>
2020-06-27 00:05:34 +0900

    [Update] UserAPIBukkit

--- bukkit/src/main/java/net/okocraft/userapi/bukkit/UserAPIBukkit.java
+++ bukkit/src/main/java/net/okocraft/userapi/bukkit/UserAPIBukkit.java
@@ -1,37 +1,35 @@
 package net.okocraft.userapi.bukkit;
 
-import com.github.siroshun09.sirolibrary.bukkitutils.BukkitUtil;
-import com.github.siroshun09.sirolibrary.config.BukkitConfig;
-import com.github.siroshun09.sirolibrary.message.BukkitMessage;
+import com.github.siroshun09.configapi.bukkit.BukkitConfig;
 import net.okocraft.userapi.UserAPIPlugin;
+import org.bukkit.event.HandlerList;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class UserAPIBukkit extends JavaPlugin {
 
     @Override
     public void onLoad() {
-        super.onLoad();
         UserAPIPlugin.init(new BukkitConfig(this, "config.yml", true), getLogger());
     }
 
     @Override
-    public void onEnable() {
-        super.onEnable();
-        if (UserAPIPlugin.isReady() && UserAPIPlugin.get().getConfig().isListenerEnabled()) {
-            BukkitUtil.registerEvents(PlayerJoinListener.get(), this);
-            BukkitMessage.printEnabledMsg(this);
+    public void onDisable() {
+        if (UserAPIPlugin.isReady()) {
+
+            if (UserAPIPlugin.get().getConfig().isListenerEnabled()) {
+                HandlerList.unregisterAll(this);
+            }
+
+            UserAPIPlugin.shutdown();
+            getLogger().info(getDescription().getName() + " v" + getDescription().getVersion() + " has been successfully disabled.");
         }
     }
 
     @Override
-    public void onDisable() {
-        super.onDisable();
-        if (UserAPIPlugin.isReady()) {
-            if (UserAPIPlugin.get().getConfig().isListenerEnabled()) {
-                BukkitUtil.unregisterEvents(this);
-                BukkitMessage.printDisabledMsg(this);
-            }
-            UserAPIPlugin.shutdown();
+    public void onEnable() {
+        if (UserAPIPlugin.isReady() && UserAPIPlugin.get().getConfig().isListenerEnabled()) {
+            getServer().getPluginManager().registerEvents(PlayerJoinListener.get(), this);
+            getLogger().info(getDescription().getName() + " v" + getDescription().getVersion() + " has been successfully enabled.");
         }
     }
 }
